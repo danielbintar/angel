ff03cd4847885f562e5f3c6cc4e8738a64e15e3c
@danielbintar <daniel_bintar@yahoo.com>
2019-05-07 21:47:31 +0700

    server

---
+++ server/gateway/cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/danielbintar/angel/server/gateway"
+)
+
+func main() {
+	s := gateway.NewServer()
+
+	h := &http.Server {
+		Addr:         ":8089",
+		Handler:      s,
+		ReadTimeout:  310 * time.Second,
+		WriteTimeout: 310 * time.Second,
+	}
+
+	log.Println("Listening to 8089")
+	if err := h.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
